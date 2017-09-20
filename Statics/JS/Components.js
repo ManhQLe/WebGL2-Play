92f5a9883e1eacd36a34c447e821eb0daa4d2dda
Manh Le <quang.manh@gmail.com>
2017-09-19 21:50:20 -0500

    Commit

---
+++ Statics/JS/Components.js
@@ -0,0 +1,11 @@
+/// <reference path="Ctrl8.js" />
+/// <reference path="Cir8.js" />
+
+function ObjLoader(init){
+    ObjLoader.baseConstructor.call(this,init);
+    this.FX = function(){
+        var 
+    }
+}
+
+CPack.ExtendsTo(ObjLoader);
